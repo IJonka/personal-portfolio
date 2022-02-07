f84d15cb02bf99789f45095b3bb96fe8aaf78e29
JavaIbr <javohiribr@gmail.com>
2022-02-07 10:10:44 +0000

    Remove old stuff

--- personal_project/settings.py
+++ personal_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tz!d^$c5(s#c7ct6tf&t7_!t=594wt%k2pj^p$1fgs(si$5^h6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['javaibr.pythonanywhere.com']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
